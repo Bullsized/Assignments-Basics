37f8be2eae9ccd65b00b17b6cfa2636d83014885
Bullsized <ivanstefanovbg@gmail.com>
2017-04-06 11:19:48 +0300

    Additional solutions

---
+++ 01 Lectures and Homeworks/01 First Steps in Coding/Square of Stars/square of stars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square_of_stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int size = int.Parse(Console.ReadLine());
+             string InitialRows = new String('*', size);
+             string WhiteSpaces = new String(' ', size - 2);
+             Console.WriteLine(InitialRows);
+
+             for (int i = 0; i < size - 2; i = i + 1)
+             {
+                 Console.Write('*');
+                 Console.Write(WhiteSpaces);
+                 Console.Write('*');
+                 Console.WriteLine();
+             }
+
+             Console.WriteLine(InitialRows);  */
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 0; i < n - 2; i++)
+                Console.Write(new string('*', 1));
+            Console.Write(new string(' ', n - 2));
+            Console.WriteLine(new string('*', 1));
+            Console.WriteLine(new string('*', n));
+
+
+        }
+    }
+}
