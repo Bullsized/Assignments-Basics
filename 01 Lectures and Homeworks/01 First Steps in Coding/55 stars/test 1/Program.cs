87c84234f15baca75767245d98cbb037c37830d4
Bullsized <ivanstefanovbg@gmail.com>
2017-04-06 11:17:25 +0300

    Creating file system
    and adding a simple project

---
+++ 01 Lectures and Homeworks/01 First Steps in Coding/55 stars/test 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("*");
+                }
+            }
+        }
+    }
+}
