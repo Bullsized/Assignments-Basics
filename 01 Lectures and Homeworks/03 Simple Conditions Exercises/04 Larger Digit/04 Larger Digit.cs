00ae5a501ce2707f9e2f406a13ff634abb91e9d1
Bullsized <ivanstefanovbg@gmail.com>
2017-04-06 11:32:53 +0300

    01 to 05

---
+++ 01 Lectures and Homeworks/03 Simple Conditions Exercises/04 Larger Digit/04 Larger Digit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Larger_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Да се напише програма, която въвежда две цели числа и отпечатва по-голямото от двете.
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if ( a > b )
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
---
+++ 01 Lectures and Homeworks/03 Simple Conditions Exercises/05 Digit with words/05 Digit with words.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Digit_with_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Да се напише програма, която въвежда цяло число в диапазона 
+            // [0…10] и го изписва с думи на английски език. Ако числото е извън диапазона, изписва “number too big”. 
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a == 0)
+            { Console.WriteLine("zero"); }
+            else if (a == 1)
+            { Console.WriteLine("one"); }
+            else if (a == 2)
+            { Console.WriteLine("two"); }
+            else if (a == 3)
+            { Console.WriteLine("three"); }
+            else if (a == 4)
+            { Console.WriteLine("four"); }
+            else if (a == 5)
+            { Console.WriteLine("five"); }
+            else if (a == 6)
+            { Console.WriteLine("six"); }
+            else if (a == 7)
+            { Console.WriteLine("seven"); }
+            else if (a == 8)
+            { Console.WriteLine("eight"); }
+            else if (a == 9)
+            { Console.WriteLine("nine"); }
+            else if (a >= 10)
+            { Console.WriteLine("number too big"); }
+        }
+    }
+}
